a9e9b72b1cdfd83844e3c4e3c6c9f34cb5272d9c
StanICT <charlestanarroyo@gmail.com>
2026-05-06 20:44:25 +0800

    fix: add missing flash, redirect, url_for imports to views.py

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for
 from flask_login import current_user, login_required
 from website.models import Product, Brand, CartItem
 from website.extensions import db
